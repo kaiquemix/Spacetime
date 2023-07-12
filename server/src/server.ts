e98cc49e9ce4862e98e090e926371269c0d86bdc
Kaique Ferreira <kaiquerbn@gmail.com>
2023-07-12 17:59:18 -0300

    final do backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,30 @@
-import fastify from 'fastify'
+import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
-const app = fastify()
+const app = fastify();
 
-//servidor HTTP - requisições
-app.listen({
+//fazer conexão com o BD
+const prisma = new PrismaClient()
+
+
+//Criar ROTA
+//pra usar o await precisa ser async (assincrona)
+//findmany - trazer varios usuarios
+app.get("/users", async () => {
+  const users = await prisma.user.findMany()
+  return users
+});
+
+//Criar APP
+//Como esse vai ser meu primeiro codigo que mostra a criação do servidor estarei comentando tudo aqui.
+//servidor HTTP - requisições para voltar a resposta.
+//listen - recebe um obj de config, sendo uma das mais importantes a PORT do servidor
+//then permite ouvir tudo quando terminar, ou seja assim que o servidor terminar ENTÃO execute isso
+
+app
+  .listen({
     port: 3333,
-}).then(() =>{
-    
-})
+  })
+  .then(() => {
+    console.log("HTTP RUNING on  local 3333😊");
+  });
