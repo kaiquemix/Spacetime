1bbd8e9d991b5fe84c80716e5cd4412cf553b1f9
Kaique Ferreira <kaiquerbn@gmail.com>
2023-07-13 17:10:16 -0300

    configuração cores font e inicio dev front

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,22 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
-      <h1 className="text-4xl font-bold">Sua Capsula do Tempo</h1>
-      <h1 className="text-4xl font-bold font-alt">Sua Capsula do Tempo</h1>
-    </div>
+    <main className="grid grid-cols-2 min-h-screen">
+      {/* LEFT */}
+      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16 border-r border-white/10">
+        {/* BLUR */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"></div>
+        {/* STRIPES */}
+        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
+      </div>
+
+      {/* RIGHT */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">Você ainda não registrou nenhuma lembrança, comece a{' '}
+          <a href="#" className="underline hover:text-gray-50">criar agora!</a></p>
+        </div>
+      </div>
+      
+    </main>
   );
 }
