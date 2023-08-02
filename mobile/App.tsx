31618cc508931f59f9c0c3b7b0abf388125e3a4f
Kaique Ferreira <kaiquerbn@gmail.com>
2023-08-02 13:21:25 -0300

    começo front mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,35 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-4xl font-bold text-zinc-50">
-        Open up App.js to start working on your app! 23242
-      </Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-black"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <Stripes />
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
